159d1a2fde680784dad727bb9fb89318e68f96b6
helto4real <tomas.hellstrom@yahoo.se>
2020-04-13 22:57:22 +0200

    More messages

--- src/bot/Bot.cs
+++ src/bot/Bot.cs
@@ -66,6 +66,9 @@ https://github.com/net-daemon/docs";
     private static Dictionary<string, string> _commandResponse = new Dictionary<string, string>
     {
         ["test"] = "What are you trying to test? Dont understand :zany_face: :zany_face:, use help for available commands.",
+        ["self destruct"] = "Ok selfdestructing in 5..4..3..2..1.. naaah, please use help for available commands.",
+        ["meaning of life"] = "42 :partying_face:, please use help for available commands.",
+        ["die"] = "Why? why? whyyyyy doo you hate meeee :sob::sob::sob:, please use help for available commands.",
         ["helto"] = "Is the weirdo that actually does this for free :rofl: :rofl:, use help for available commands.",
         ["ludeeus"] = "Hangaround dev dunno what he reallys does for a living :grimacing:, use help for available commands.",
         ["netdaemon"] = "Yes you have come to the right server not try a better command :kissing_closed_eyes:, use help for available commands.",
